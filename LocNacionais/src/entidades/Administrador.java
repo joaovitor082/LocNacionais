a30573cb5bdf2f8530a093f8039ea324229d8501
joaovitor082 <147451629+joaovitor082@users.noreply.github.com>
2024-10-31 20:54:54 -0300

    Administrador Update

--- LocNacionais/src/entidades/Administrador.java
+++ LocNacionais/src/entidades/Administrador.java
@@ -1,5 +1,19 @@
 package entidades;
 
 public class Administrador {
-  
+    private String nome;
+    private int senha;
+
+    public Administrador(String nome, int senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+      return nome;
+    }
+
+    public int getSenha() {
+      return senha;
+    }
 }
