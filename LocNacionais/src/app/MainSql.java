747f698655b07bae599ceb7f0b1747e9ee50720a
joaovitor082 <147451629+joaovitor082@users.noreply.github.com>
2024-11-01 00:20:59 -0300

    update

--- LocNacionais/src/app/MainSql.java
+++ LocNacionais/src/app/MainSql.java
@@ -1,5 +1,16 @@
 package app;
 
+import java.sql.Connection;
+
+import util.ConexaoMySql;
+
 public class MainSql {
-  
+   public static void main(String[] args) {
+      Connection conexao = ConexaoMySql.conectar();
+      if (conexao != null) {
+         System.out.println("Conectado com sucesso!");
+      } else {
+         System.out.println("Erro ao conectar!");
+      }
+    }  
 }
--- LocNacionais/src/util/ConexaoMySql.java
+++ LocNacionais/src/util/ConexaoMySql.java
@@ -5,5 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoMySql {
-  
+  private static final String URL = "jdbc:mysql://localhost:3306/locadora";
+  private static final String USUARIO = "root";
+  private static final String SENHA = "92537602Lin@";
+
+  public static Connection conectar() {
+    Connection conexao = null;
+    try {
+      conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+    } catch (SQLException e) {
+      System.out.println("Erro ao conectar: " + e.getMessage());
+    }
+    return conexao;
+  }
+
 }
